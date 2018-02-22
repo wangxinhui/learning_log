0dd4fdbec2cc892b078ecb899c9b26dbcb7dbac9
wangxinhui <wangxinhuileft@163.com>
2018-02-22 15:45:44 +0800

    chapter  19

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,8 +5,9 @@ urlpatterns =[
 	# home
 	url(r'^$',views.index,name='index'),
 	# 显示所有主题
-url(r'^topics/$',views.topics,name='topics'),
+	url(r'^topics/$',views.topics,name='topics'),
 	url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topics'),
+	url(r'^new_topics/$',views.new_topic,name='new_topics'),
 ]
 
 app_name='learning_logs'
