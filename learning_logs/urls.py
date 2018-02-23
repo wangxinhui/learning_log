08e86f97230eeeec36a5dbf6acd40beb8b4a7cd9
wangxinhui <wangxinhuileft@163.com>
2018-02-23 17:32:41 +0800

    chapter  19

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,8 +6,9 @@ urlpatterns =[
 	url(r'^$',views.index,name='index'),
 	# 显示所有主题
 	url(r'^topics/$',views.topics,name='topics'),
-	url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topics'),
-	url(r'^new_topics/$',views.new_topic,name='new_topics'),
+	url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+	url(r'^new_topic/$',views.new_topic,name='new_topic'),
+	url(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
 ]
 
 app_name='learning_logs'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Topic
+from .forms import TopicForm,EntryForm
 
 # Create your views here.
 def index(request):
@@ -14,3 +18,28 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('date_added')
     content = {'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',content)
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic_id]))
+    context = {'topic':topic,'form':form}
+    return render(request,'learning_logs/new_entry.html',context)
+
